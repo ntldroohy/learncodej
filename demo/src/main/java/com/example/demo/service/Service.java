8b67d420d6f93fadfeeda14f4bc9115f3ac70ce6
yangyj <506666243@qq.com>
2022-05-10 13:48:46 +0800

    test dev

---
+++ demo/src/main/java/com/example/demo/service/Service.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+/**
+ * @author yangyj
+ * @description
+ * @create 2022年05月10日 13:37
+ * @project learncodej
+ */
+public interface Service {
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/ServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;/**
+    @description
+    @author yangyj
+    @create 2022年05月10日 13:38
+    @project learncodej
+    @version
+*/public class ServiceImpl {
+}
